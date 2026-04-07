182e982d7eb6fe52e1cc49cebf1b263a3ff1ce4e
Daniel Jones <daniel@1994.co.uk>
2026-04-07 13:38:53 +0100

    Fix #208

--- src/Http/Controllers/Web/SitemapController.php
+++ src/Http/Controllers/Web/SitemapController.php
@@ -99,6 +99,6 @@ class SitemapController extends LaravelController
     {
         $path = __DIR__ . '/../../../../composer.json';
         $contents = file_get_contents($path);
-        return json_decode($contents, true)['version'];
+        return json_decode($contents, true)['version'] ?? 'unknown';
     }
 }
